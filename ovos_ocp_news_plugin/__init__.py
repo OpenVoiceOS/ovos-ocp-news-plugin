03fa77beb0c7caf1a58b1066b75f6fe0318a1c25
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-02-17 04:58:34 +0000

    fix/gpb + unittests (#5)
    * feat/unittests
    
    * latest OCP ofr unittests
    
    * missing requirements.txt
    
    * fix/gbp

--- ovos_ocp_news_plugin/__init__.py
+++ ovos_ocp_news_plugin/__init__.py
@@ -1,6 +1,4 @@
-import feedparser
 import pytz
-import re
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -8,6 +6,7 @@ from datetime import timedelta
 from lingua_franca.time import now_local
 from ovos_ocp_rss_plugin import OCPRSSFeedExtractor
 from ovos_plugin_manager.templates.ocp import OCPStreamExtractor
+from ovos_utils.log import LOG
 from pytz import timezone
 from urllib.request import urlopen
 
@@ -45,8 +44,9 @@ class OCPNewsExtractor(OCPStreamExtractor):
 
     def extract_stream(self, uri, video=True):
         """ return the real uri that can be played by OCP """
+        meta = {}
         if uri.startswith("news//"):
-            uri = uri[6:]
+            uri = meta["uri"] = uri[6:]
         if uri.startswith(self.NPR_URL):
             return self.npr()
         elif uri.startswith(self.TSF_URL):
@@ -59,6 +59,7 @@ class OCPNewsExtractor(OCPStreamExtractor):
             return self.ft()
         elif uri.startswith(self.ABC_URL):
             return self.abc()
+        return meta  # dropped the news// sei if present
 
     @classmethod
     def tsf(cls):
@@ -84,23 +85,10 @@ class OCPNewsExtractor(OCPStreamExtractor):
     @classmethod
     def gpb(cls):
         """Custom news fetcher for GPB news."""
-        feed = f'{cls.GPB_URL}/GeorgiaRSS?format=xml'
-        data = feedparser.parse(feed)
-        next_link = None
-        for entry in data['entries']:
-            # Find the first mp3 link with "GPB {time} Headlines" in title
-            if 'GPB' in entry['title'] and ('Headlines' in entry['title']):
-                next_link = entry['links'][0]['href']
-                break
-        html = requests.get(next_link)
-        # Find the first mp3 link
-        # Note that the latest mp3 may not be news,
-        # but could be an interview, etc.
-        mp3_find = re.search(r'href="(?P<mp3>.+\.mp3)"'.encode(), html.content)
-        if mp3_find is None:
-            return None
-        uri = mp3_find.group('mp3').decode('utf-8')
-        return {"uri": uri, "title": "GPB News", "author": "GPB"}
+        # https://www.gpb.org/radio/programs/georgia-today
+        LOG.debug("requested GBP feed has been deprecated, automatically mapping to Georgia Today")
+        url = "https://gpb-rss.streamguys1.com/gpb/georgia-today-npr-one.xml"
+        return OCPRSSFeedExtractor.get_rss_first_stream(url)
 
     @classmethod
     def npr(cls):
@@ -165,20 +153,3 @@ class OCPNewsExtractor(OCPStreamExtractor):
         return {"uri": url,
                 "title": "ABC News Australia",
                 "author": "Australian Broadcasting Corporation"}
-
-
-if __name__ == "__main__":
-    # dedicated parsers
-    "http://www.rtp.pt//play/itunes/5442"
-    print(OCPRSSFeedExtractor.get_rss_first_stream("rss//https://www.rtp.pt/rdpafrica/rss"))
-    exit()
-    print(OCPNewsExtractor.npr())
-    print(OCPNewsExtractor.tsf())
-    print(OCPNewsExtractor.gr1())
-    print(OCPNewsExtractor.gpb())
-    print(OCPNewsExtractor.ft())
-    print(OCPNewsExtractor.abc())
-    # RSS
-    print(OCPRSSFeedExtractor.get_rss_first_stream("rss//https://www.cbc.ca/podcasting/includes/hourlynews.xml"))
-    print(OCPRSSFeedExtractor.get_rss_first_stream("rss//https://podcasts.files.bbci.co.uk/p02nq0gn.rss"))
-    print(OCPRSSFeedExtractor.get_rss_first_stream("rss//https://www.pbs.org/newshour/feeds/rss/podcasts/show"))
---
+++ test/unittests/test_sei_plugins.py
@@ -0,0 +1,27 @@
+import unittest
+
+from ovos_plugin_manager.ocp import StreamHandler
+
+
+class TestOCPExtractor(unittest.TestCase):
+
+    def test_news(self):
+        parser = StreamHandler()
+        news_urls = [
+            "https://www.raiplaysound.it",
+            "https://www.tsf.pt/stream",
+            "https://www.abc.net.au/news",
+            "https://www.ft.com",
+            "https://www.npr.org/rss/podcast.php",
+            "http://feeds.feedburner.com/gpbnews"  # deprecated, mapped to GeorgiaToday
+        ]
+        for url in news_urls:
+            print(f"#### {url}")
+            meta = parser.extract_stream(url) or {}
+            self.assertTrue(bool(meta.get("uri")))
+            meta = parser.extract_stream(f"news//{url}") or {}
+            self.assertTrue(bool(meta.get("uri")))
+
+
+if __name__ == '__main__':
+    unittest.main()
