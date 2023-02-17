7d4603ba866e314c094d2ba1cc52314421bb6102
jarbasai <jarbasai@mailfence.com>
2023-02-17 05:42:15 +0000

    test NPR newcasts

--- test/unittests/test_sei_plugins.py
+++ test/unittests/test_sei_plugins.py
@@ -12,12 +12,26 @@ class TestOCPExtractor(unittest.TestCase):
             "https://www.tsf.pt/stream",
             "https://www.abc.net.au/news",
             "https://www.ft.com",
+            "http://feeds.feedburner.com/gpbnews",  # deprecated, mapped to GeorgiaToday
             "https://www.npr.org/rss/podcast.php",
-            "http://feeds.feedburner.com/gpbnews"  # deprecated, mapped to GeorgiaToday
+            "https://www.npr.org/podcasts/500005/npr-news-now",
+            "https://www.npr.org/podcasts/828054805/alaska-news-nightly",
+            "https://www.npr.org/podcasts/381444103/k-h-n-s-f-m-local-news",
+            "https://www.npr.org/podcasts/1111549375/k-g-o-u-p-m-news-brief",
+            "https://www.npr.org/podcasts/1111549080/k-g-o-u-a-m-news-brief",
+            "https://www.npr.org/podcasts/1100476310/aspen-public-radio-newscast",
+            "https://www.npr.org/podcasts/1090302835/first-news",
+            "https://www.npr.org/podcasts/1071428476/n-h-news-recap",
+            "https://www.npr.org/podcasts/1074915520/n-s-p-r-headlines",
+            "https://www.npr.org/podcasts/1038076755/w-s-i-u-news-updates",
+            "https://www.npr.org/podcasts/1031233995/s-d-p-b-news",
+            "https://www.npr.org/podcasts/1033362253/the-midday-news-report"
+
         ]
         for url in news_urls:
             print(f"#### {url}")
             meta = parser.extract_stream(url) or {}
+            print(meta)
             self.assertTrue(bool(meta.get("uri")))
             meta = parser.extract_stream(f"news//{url}") or {}
             self.assertTrue(bool(meta.get("uri")))
