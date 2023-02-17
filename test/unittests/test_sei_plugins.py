d6bec4e13e4936a1881ff16204dfb75c8b5d4aad
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-02-17 06:03:43 +0000

    add NPR newcasts (#6)
    * add NPR newcasts
    
    * test NPR newcasts
    
    * move extractors to own file
    
    * url mappings

--- test/unittests/test_sei_plugins.py
+++ test/unittests/test_sei_plugins.py
@@ -1,23 +1,17 @@
 import unittest
 
 from ovos_plugin_manager.ocp import StreamHandler
+from ovos_ocp_news_plugin.extractors import URL_MAPPINGS
 
 
 class TestOCPExtractor(unittest.TestCase):
 
     def test_news(self):
         parser = StreamHandler()
-        news_urls = [
-            "https://www.raiplaysound.it",
-            "https://www.tsf.pt/stream",
-            "https://www.abc.net.au/news",
-            "https://www.ft.com",
-            "https://www.npr.org/rss/podcast.php",
-            "http://feeds.feedburner.com/gpbnews"  # deprecated, mapped to GeorgiaToday
-        ]
-        for url in news_urls:
+        for url in URL_MAPPINGS:
             print(f"#### {url}")
             meta = parser.extract_stream(url) or {}
+            print(meta)
             self.assertTrue(bool(meta.get("uri")))
             meta = parser.extract_stream(f"news//{url}") or {}
             self.assertTrue(bool(meta.get("uri")))
